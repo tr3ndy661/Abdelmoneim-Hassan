ffa0ccb41d65a52d252ccae677df3675eb561a17
Abdelmoneim <trendypotato661@gmail.com>
2026-08-12 00:28:32 +0300

    .

--- assets/product-grid.js
+++ assets/product-grid.js
@@ -56,8 +56,6 @@
     function closeAllPopups() {
       popups.forEach(function (popup) {
         popup.setAttribute('aria-hidden', 'true');
-        var gridItem = popup.closest('.product-grid__item');
-        if (gridItem) gridItem.style.zIndex = '';
       });
       hotspots.forEach(function (btn) {
         btn.setAttribute('aria-expanded', 'false');
@@ -88,7 +86,6 @@
       /* If this popup was closed, open it now */
       if (!isCurrentlyOpen) {
         popup.setAttribute('aria-hidden', 'false');
-        if (gridItem) gridItem.style.zIndex = '9999';
         if (hotspot) hotspot.setAttribute('aria-expanded', 'true');
         if (backdrop) backdrop.classList.add('product-grid__backdrop--visible');
       }
